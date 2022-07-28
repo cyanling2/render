ffa7ebaa260a470843b97b46e9c3e53b8697f26b
cyanling2 <ling.24@foxmail.com>
2022-07-28 12:54:56 -0700

    enable animation

--- pyblender_nodocker.py
+++ pyblender_nodocker.py
@@ -13,6 +13,7 @@ gpu=int(sys.argv[-1])
 num_gpus=int(sys.argv[-2])
 end=int(sys.argv[-4])
 start=int(sys.argv[-3])
+animation=int(sys.argv[-5])
 
 for i in range(len(devices)):
     devices[i].use = (False and i!=gpu) or (True and i==gpu)
@@ -27,21 +28,23 @@ print(start, end)
 
 
 scn = bpy.context.scene
-# scn.render.image_settings.file_format="PNG"
 scn.render.filepath="./nodocker_"+str(gpu)
 scn.render.resolution_x = 1280
 scn.render.resolution_y = 720
 scn.render.resolution_percentage = 100
-# scn.render.use_border=True
-# scn.render.border_min_x = 0
-# scn.render.border_max_x = 0.125
-# scn.render.border_min_y = 0
-# scn.render.border_max_y = 1
+if not animation:
+    scn.render.use_border=True
+    scn.render.border_min_x = MINX
+    scn.render.border_max_x = MAXX
+    scn.render.border_min_y = MINY
+    scn.render.border_max_y = MAXY
+    scn.frame_set(100)
+    scn.render.image_settings.file_format="PNG"
+else:
+    scn.frame_start = start
+    scn.frame_end = end
 scn.render.engine="CYCLES"
 scn.cycles.device='GPU'
 bpy.context.scene.cycles.device = "GPU"
-# scn.frame_set(100)
-scn.frame_start = start
-scn.frame_end = end
-bpy.ops.render.render(animation=True, write_still=True)
+bpy.ops.render.render(animation=animation, write_still=True)
 end=time.time()
